ef3740abb612001babb2d430c2d54b302d932fb5
MelForze <MelForze@users.noreply.github.com>
2026-02-19 02:04:00 +0300

    fix

--- honeycore/__init__.py
+++ honeycore/__init__.py
@@ -7,6 +7,6 @@ from importlib import metadata
 try:
     __version__ = metadata.version("redposture")
 except metadata.PackageNotFoundError:
-    __version__ = "0.1.0+local"
+    __version__ = "1.0.2+local"
 
 __all__ = ["__version__"]
